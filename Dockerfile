1b6fe320b930d7dbda9fda5832d6e841b3fe8bce
v.dauzhuk <64989065+vandalv@users.noreply.github.com>
2023-09-06 01:17:01 +0200

    changes according review

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,4 @@
-FROM gradle:8.1.1-jdk17 AS build
-COPY --chown=gradle:gradle . /usr/src/app
-WORKDIR /usr/src/app
-RUN gradle build --no-daemon
-
 FROM openjdk:17-alpine
 WORKDIR /app
-COPY --from=build /usr/src/app/build/libs/*.jar /app/
+COPY ./build/libs/country-city-0.0.1-SNAPSHOT.jar /app/
 CMD ["java", "-jar", "country-city-0.0.1-SNAPSHOT.jar"]
--- src/test/java/com/andersen/countrycity/controller/impl/CityControllerImplTest.java
+++ src/test/java/com/andersen/countrycity/controller/impl/CityControllerImplTest.java
@@ -1,58 +1,85 @@
 package com.andersen.countrycity.controller.impl;
 
 import com.andersen.countrycity.data.TestDataFactory;
+import com.andersen.countrycity.dto.AuthenticationRequestDTO;
+import com.andersen.countrycity.dto.AuthenticationResponseDTO;
 import com.andersen.countrycity.dto.CityDTO;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 @ActiveProfiles("test")
 class CityControllerImplTest {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:latest"
+    ).withInitScript("init.sql");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @LocalServerPort
+    private int randomServerPort;
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     private String baseUrl;
     private String countryNameParameterValue;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-
     @BeforeEach
-    public void setUp(){
-
-        baseUrl = TestDataFactory.baseUrl();
+    public void setUp() {
+        baseUrl = "http://localhost:" + randomServerPort;
         countryNameParameterValue = TestDataFactory.countryNameParameterValue();
     }
 
     @Test
     void getCitiesByCountry() {
+        String url = baseUrl + "/cities/country?name=" + countryNameParameterValue;
 
-        String url = baseUrl + "8737" + "/cities/country?name=" + countryNameParameterValue;
+        List<CityDTO> citiesByCountry = webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(CityDTO.class)
+                .returnResult()
+                .getResponseBody();
 
-        ResponseEntity<List<CityDTO>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<CityDTO>>() {});
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        List<CityDTO> citiesByCountry = response.getBody();
         assertNotNull(citiesByCountry);
 
         List<String> expectedCityNames = Arrays.asList("Brest", "Lille", "Marselle", "Paris");
@@ -60,19 +87,107 @@ class CityControllerImplTest {
                 .map(CityDTO::getName)
                 .toList();
 
-        Assertions.assertIterableEquals(expectedCityNames, actualCityNames);
+        assertEquals(expectedCityNames, actualCityNames);
     }
 
     @Test
     void getUniqueCityNames() {
+        String url = baseUrl + "/cities/unique";
 
-        ResponseEntity<List> response = restTemplate.getForEntity(
-                baseUrl + "8737" + "/cities/unique",
-                List.class);
+        List<String> uniqueCityNames = webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(String.class)
+                .returnResult()
+                .getResponseBody();
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        List<String> uniqueCityNames = response.getBody();
         assertNotNull(uniqueCityNames);
+    }
+
+    @Test
+    void getCitiesByCountry_ValidCountryName_ShouldReturnCityList() {
+
+        String validCountryName = "France";
+        String url = baseUrl + "/cities/country?name=" + validCountryName;
+
+        List<CityDTO> citiesByCountry = webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(CityDTO.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertNotNull(citiesByCountry);
+        assertFalse(citiesByCountry.isEmpty());
+    }
+
+    @Test
+    void getCitiesByCountry_InvalidCountryName_ShouldReturnEmptyList() {
+
+        String invalidCountryName = "NonExistentCountry";
+        String url = baseUrl + "/cities/country?name=" + invalidCountryName;
+
+        List<CityDTO> citiesByCountry = webTestClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(CityDTO.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertNotNull(citiesByCountry);
+        assertTrue(citiesByCountry.isEmpty());
+    }
+
+    private String authenticateAndGetBearerToken(String email, String password) {
+        AuthenticationRequestDTO authRequest = new AuthenticationRequestDTO();
+        authRequest.setEmail(email);
+        authRequest.setPassword(password);
+
+        EntityExchangeResult<AuthenticationResponseDTO> authenticationResponse = webTestClient
+                .post()
+                .uri("users/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(authRequest)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AuthenticationResponseDTO.class)
+                .returnResult();
+
+        String bearerToken = authenticationResponse.getResponseBody().getToken();
+
+        return "Bearer " + bearerToken;
+    }
+
+    private void testUpdateCityWithToken(String email, String password, Long cityId, String newName,
+            String newLogo, HttpStatus expectedStatus) {
+
+        String jwtToken = authenticateAndGetBearerToken(email, password);
+
+        webTestClient
+                .put()
+                .uri("/cities/" + cityId + "/edit?name=" + newName + "&logo=" + newLogo)
+                .header("Authorization", jwtToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(expectedStatus);
+    }
+
+    @Test
+    void updateCityNameAndLogo_WithValidBearerToken_ShouldReturnOk() {
+        testUpdateCityWithToken("2@example.com", "12345", 1L, "Paris", "qBw9snD/Szczecin-PL.png",
+                HttpStatus.OK);
+    }
+
+    @Test
+    void updateCityNameAndLogo_WithInvalidBearerToken_ShouldReturnForbidden() {
+        testUpdateCityWithToken("1@example.com", "12345", 1L, "Paris", "qBw9snD/Szczecin-PL.png",
+                HttpStatus.FORBIDDEN);
     }
 }
